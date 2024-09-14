ae8060ed34a51546d88886c796a9e0636d3d1484
Nathan3-14 <nathan.watson.172@gmail.com>
2024-09-14 09:00:55 +0100

    Fixed merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,8 @@
-def merge_sort(_unordered_list):
-    _list_old = [[element] for element in _unordered_list]
+def merge_sort(_unordered_list, first: bool=False):
+    if first:
+        _list_old = [[element] for element in _unordered_list]
+    else:
+        _list_old = _unordered_list
     _list_new = []
 
     for index in range(0, len(_list_old), 2):
@@ -8,36 +11,39 @@ def merge_sort(_unordered_list):
         index_1 = 0
         index_2 = 0
         while True:
-            if _list_old[index][index_1] <= _list_old[index+1][index_2]:
-                to_add.append(_list_old[index][index_1])
+            try:
+                _list_old[index+1]
+            except IndexError:
+                for item in _list_old[index]:
+                    to_add.append(item)
+                break
+
+            if index_1 >= len(_list_old[index]):
+                for index_2_offset in range(len(_list_old[index+1])-index_2):
+                    to_add.append(_list_old[index+1][index_2+index_2_offset])
+                break
+            if index_2 >= len(_list_old[index+1]):
+                for index_1_offset in range(len(_list_old[index])-index_1):
+                    to_add.append(_list_old[index][index_1+index_1_offset])
+                break
+
+            current_1 = _list_old[index][index_1]
+            current_2 = _list_old[index+1][index_2]
+
+            if current_1 <= current_2:
+                to_add.append(current_1)
                 index_1 += 1
             else:
-                to_add.append(_list_old[index+1][index_2])
+                to_add.append(current_2)
                 index_2 += 1
-            
-            if index_1 >= len(_list_old[index]):
-                while index_2 < len(_list_old[index+1]):
-                    to_add.append(_list_old[index+1][index_2])
-                    index_2 += 1
-                break
-            elif index_1 >= len(_list_old[index]):
-                while index_1 < len(_list_old[index]):
-                    to_add.append(_list_old[index][index_1])
-                    index_1 += 1
-                break
 
         _list_new.append(to_add)
 
-    # _list_new = [_list_old[index] + _list_old[index+1] for index in range(0, len(_list_old), 2)]
-
-
-    print(_list_new)
-
     if len(_list_new) == 1:
         print("Done!")
-        return _list_new
+        return _list_new[0]
     else:
         return merge_sort(_list_new)
 
 if __name__ == "__main__":
-    print(merge_sort([1, 3, 6, 1, 2, 10, 14, 4]))
+    print(merge_sort([1, 3, 6, 1, 2, 10, 14, 4], True))
--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@ import random
 from binary_search import binary_search
 from linear_search import linear_search
 
+from merge_sort import merge_sort
+
 ordered_list = list(range(1, 101))
 unordered_list = ordered_list.copy()
 random.shuffle(unordered_list)
@@ -10,3 +12,7 @@ linear_search(unordered_list, 57)
 binary_search(ordered_list, 57)
 linear_search(unordered_list, 107)
 binary_search(ordered_list, 107)
+
+print(
+    merge_sort(unordered_list, True)
+)
