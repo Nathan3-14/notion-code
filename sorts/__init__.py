7f5e6b686a4784f3affa89e0d853930450167fd9
Nathan3-14 <nathan.watson.172@gmail.com>
2024-09-19 17:13:57 +0100

    Added tests

--- sorts/__init__.py
+++ sorts/__init__.py
@@ -1,2 +1,3 @@
 from .bubble_sort import bubble_sort
 from .merge_sort import merge_sort
+from .insertion_sort import insertion_sort
--- tests.py
+++ tests.py
@@ -1,7 +1,19 @@
+import datetime
 import random
 
 from searches import linear_search, binary_search
-from sorts import bubble_sort, merge_sort
+from sorts import bubble_sort, merge_sort, insertion_sort
+
+start = None
+def time_stamp(message: str=""):
+    global start
+    if start == None:
+        start = datetime.datetime.now()
+    else:
+        end = datetime.datetime.now()
+        time_delta = end - start
+        print(f"{message} Took {time_delta.microseconds / 1000} milliseconds")
+        start = None
 
 #* List Setup *#
 ordered_list = list(range(1, 101))
@@ -15,9 +27,22 @@ linear_search(unordered_list, 107)
 binary_search(ordered_list, 107)
 
 #* Run Sorts *#
+time_stamp()
 print(
     bubble_sort(unordered_list)
 )
+time_stamp()
+
+
+time_stamp()
 print(
     merge_sort(unordered_list, True)
 )
+time_stamp()
+
+
+time_stamp()
+print(
+    insertion_sort(unordered_list)
+)
+time_stamp()
