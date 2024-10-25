c3ec0637597e8e08d952a83ed3772e6d8b759c9b
nathan.watson.172 <nathan.watson.172@gmail.com>
2024-10-25 14:04:11 +0000

    addedslots file

---
+++ home_work_temp/slots.py
@@ -0,0 +1,18 @@
+from random import choice
+
+
+
+ROLLS = {
+    "1": 5,
+    "2": 5,
+    "3": 5,
+    "4": 3,
+    "5": 1
+}
+COST = 1
+money = 10
+
+while money > 0:
+    pass
+
+print("PLAY AGAIN!")
