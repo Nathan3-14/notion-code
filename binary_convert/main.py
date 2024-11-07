67ec132d0da9eebf06b2002e05e370ca515a947a
nathan.watson.172 <nathan.watson.172@gmail.com>
2024-11-07 14:33:50 +0000

    stuff

--- binary_convert/main.py
+++ binary_convert/main.py
@@ -1,4 +1,4 @@
-from math import floor
+from math import floor, ceil
 
 def binary_to_denary(binary: str) -> int:
     current = 0
@@ -7,6 +7,37 @@ def binary_to_denary(binary: str) -> int:
         if char == "1":
             current += 1
     return current
+
+hex_binary_lookup = {
+    "0000": "0",
+    "0001": "1",
+    "0010": "2",
+    "0011": "3",
+    "0100": "4",
+    "0101": "5",
+    "0110": "6",
+    "0111": "7",
+    "1000": "8",
+    "1001": "9",
+    "1010": "A",
+    "1011": "B",
+    "1100": "C",
+    "1101": "D",
+    "1110": "E",
+    "1111": "F"
+}
+def binary_to_hex(binary: str) -> str:
+    binary_lenth = ceil(len(binary)/4)
+    binary = binary.rjust(binary_lenth*4, "0")
+    to_return = []
+    
+    for i in range(binary_lenth):
+        current = binary[0+4*i:4+4*i]
+        # print(f"  {current}")
+        to_return.append(hex_lookup[current])
+    return "".join(to_return)
+
+
 
 def denary_to_binary(denary: int) -> str:
     current = []
@@ -19,6 +50,23 @@ def denary_to_binary(denary: int) -> str:
         [str(item) for item in current]
     )
 
+def denary_to_hex(denary: int) -> str:
+    return binary_to_hex(denary_to_binary(denary))
+
+def hex_to_binary(_hex: str) -> str:
+    for char in _hex:
+        
+
+denary_hex_lookup = {
+    "A": 
+}
+def hex_to_denary(_hex: str) -> int:
+    to_return = []
+    for char in _hex:
+
+
 if __name__ == "__main__":
     print(binary_to_denary("1011"))
     print(denary_to_binary(11))
+    print(binary_to_hex("101001"))
+    print(hex_to_binary("29"))
