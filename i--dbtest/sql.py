68352ed874ea8c1aadf8106ef4e1dcbeaa54ea26
nathan.watson.172 <nathan.watson.172@gmail.com>
2024-11-22 15:28:37 +0000

    Added sql stuff

---
+++ i--dbtest/sql.py
@@ -0,0 +1,45 @@
+import sqlite3 as sq3
+from sys import argv
+
+class Main:
+    def __init__(self, file_address: str) -> None:
+        self.connection = sq3.connect(file_address)
+        self.cursor = self.connection.cursor()
+
+    def console(self, *args) -> int:
+        try:
+            _input = ""
+            while _input not in ["quit", "exit", "end"]:
+                _input = input(">> ")
+                self.cursor.execute(_input)
+        except Exception as e:
+            print(e)
+            return -1
+        return 0
+    
+    def display(self, *args) -> int:
+        if len(args) != 1:
+            print("BAD :( args")
+        try:
+            self.cursor.execute(f"SELECT * FROM {args[0]}").fetchall()
+        except Exception as e:
+            print(e)
+            return -1
+
+        return 0
+
+if __name__ == "__main__":
+    main = Main("./shop.db")
+    args = argv[1:]
+    commands = {
+        "console": main.console,
+        "list": main.display
+    }
+
+    if not len(args) >= 1:
+        print(f"Invalid run code, requires one of ({', '.join(list(commands.keys()))}) to run")
+        quit()
+    
+    main.commands[args[0]](args[1:])
+
+# cursor.execute("CREATE TABLE orders (OrderId INT, CustomerId INT, DateShipped DATE)")
