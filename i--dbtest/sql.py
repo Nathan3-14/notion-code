08d0884d998f5518c888a15cd3cd4735356a5681
nathan3-14 <nathan.watson.172@gmail.com>
2024-11-24 18:31:33 +0000

    Added preveiwer

--- i--dbtest/sql.py
+++ i--dbtest/sql.py
@@ -28,23 +28,53 @@ class Main:
             return -1
         return 0
     
-    def display(self, *args) -> int:
+    def display(self, args) -> int:
         if len(args) != 1:
-            print("BAD :( args")
+            print("BAD args :(")
+            return -1
+        database_name = args[0]
         try:
-            self.cursor.execute(f"SELECT * FROM {args[0]}").fetchall()
+            self.cursor.execute(f"SELECT * FROM {database_name}")
+            print(self.cursor.fetchall())
         except Exception as e:
             print(e)
             return -1
 
         return 0
 
+    def create_table(self, args) -> int:
+        if len(args) != 1:
+            print("BAD args :(")
+            return -1
+        file_path = args[0]
+        try:
+            with open(file_path, "r") as f:
+                data = f.readlines()
+            table_name = data[0]
+            table_keys = data[1].split(",")
+            table_types = data[2].split(",")
+            table_extras = [extra_list.split(".") for extra_list in data[3].split(",")]
+
+            command = f"CREATE TABLE {table_name} ( {''.join([
+                f'{table_keys[index]} {table_types[index]} {' '.join(table_extras[index])},'
+                for index in range(len(table_keys))
+            ]).strip(',')} );"
+            print(command)
+            self.cursor.execute(command)
+            
+        except Exception as e:
+            print(e)
+            return -1
+        return 0
+    
+
 if __name__ == "__main__":
     main = Main("./shop.db")
     args = argv[1:]
     commands = {
         "console": main.console,
-        "list": main.display
+        "list": main.display,
+        "create": main.create_table
     }
 
     if not len(args) >= 1:
