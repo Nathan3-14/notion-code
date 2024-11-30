5434bb12a2371f2afbdbb55b26385d7a7c13db2c
nathan.watson.172 <nathan.watson.172@gmail.com>
2024-11-30 11:43:56 +0000

    Fixed email sender

--- emlsender/spam.py
+++ emlsender/spam.py
@@ -1,16 +1,23 @@
 import smtplib
+from rich import print
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 587)
+usrpass = open("./s--pswd.txt", "r").readlines()
+gmail_user = usrpass[0].strip()
+gmail_app_password = usrpass[1].strip()
+
+
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.ehlo()
 
-sent_from = "nathan.watson@oasisbrislington.org"
+sent_from = gmail_user
 sent_to = "nathan.watson@oasisbrislington.org"
 
 
-server.login("nathan.watson@oasisbrislington.org", "qqbz hbby ixne ascd")
+server.login(gmail_user, gmail_app_password)
 print("[bright_green bold]Logged in[/bright_green bold]")
 
 server.sendmail(sent_from, sent_to, "hi!")
-print(f"[bright_cyan]Sent email to {to}[/bright_cyan]")
+print(f"[bright_cyan]Sent email to {sent_to}[/bright_cyan]")
 
 server.close()
+
